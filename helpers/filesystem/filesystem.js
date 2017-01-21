1be5884dfa6bfdd003a443ad4d01438b655d29a1
kiricon <kiricon@live.com>
2017-01-21 11:55:39 -0700

    Added comment for walkSync method I had forgot.

--- helpers/filesystem/filesystem.js
+++ helpers/filesystem/filesystem.js
@@ -1,6 +1,14 @@
 const path = require('path');
 const fs = require('fs');
 
+
+/**
+ * Function for recursively finding files in a provided path
+ * and returning the absolute paths in a string array.
+ * @param {String} dir - Directory to recursively look in
+ * @param {String[]} filelist - Nullable array of files to add too
+ * @return {String[]} - Abosolute path of all files in folder
+ */
 module.exports.walkSync = function(dir, filelist) {
     let files = fs.readdirSync(dir);
     filelist = filelist || [];
