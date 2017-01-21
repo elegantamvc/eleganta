ba604525a3ff032c36fcb90204ab3abedb48dfbf
kiricon <kiricon@live.com>
2017-01-20 18:58:55 -0700

    We are now returning just usefull cli arguments.

--- cli/cli.js
+++ cli/cli.js
@@ -1,3 +1,20 @@
-module.exports.help= function() {
-    console.log('you are trying to serve the website');
-}
+
+/**
+ * Helper function for parsing out the noise
+ * of the command line arguments and returning
+ * just the eleganta commands
+ * @param {String[]} args - Array of arguments from the terminal
+ * @return {String[]} - Shortend array of just useful string for commands
+ */
+module.exports.parseOutArguments = function(args) {
+    let commandIndex = 0;
+
+    for(let i = 0; i < args.length; i ++) {
+        if(args[i].indexOf('eleganta') > -1 || args[i].indexOf('core') > -1) {
+            commandIndex = i+1;
+            break;
+        }
+    }
+
+    return args.splice(commandIndex, args.length);
+};
--- core.js
+++ core.js
@@ -1,1 +1,6 @@
-console.log(process.argv);
+const Cli = require('./cli/cli');
+const args = process.argv;
+
+console.log(Cli.parseOutArguments(args));
+
+
