b9583ed996e38d2eb8eb296e40c0df2c36669bc9
kiricon <kiricon@live.com>
2017-01-21 11:04:30 -0700

    Built out remaing http paths and comments

--- core/route/route.js
+++ core/route/route.js
@@ -22,46 +22,61 @@ class Route {
     /**
      * Our HTTP GET method for setting up a new routes
      * linked to a controller
-     * @param {String} path - Url path of the webapp to point to;
+     * @param {String} path - Url path of the webapp to point to
      * @param {String} controllerString - String containing controller and
      * action  seperated by an @;
      */
     get(path, controllerString) {
-
         this.express.get(path, this.getControllerMethod(controllerString));
     }
 
+    /**
+     * Our HTTP PUT method for setting up a new routes
+     * @param {String} path - url path of the webapp to point to
+     * @param {String} controllerString
+     */
     put(path, controllerString) {
-        this.express.put(path, getControllerMethod(controllerString));
+        this.express.put(path, this.getControllerMethod(controllerString));
     }
 
+    /**
+     * Our HTTP POST method for setting up a new routes
+     * @param {String} path - url path of the webapp to point to
+     * @param {String} controllerString
+     */
     post(path, controllerString) {
-
-
-        this.express.post(path, control[controllerConfig.method]);
+        this.express.post(path, this.getControllerMethod(controllerString));
     }
 
+    /**
+     * Our HTTP DELETE method for setting up a new routes
+     * @param {String} path - url path of the webapp to point to
+     * @param {String} controllerString
+     */
     delete(path, controllerString) {
-        let controllerConfig = this.parseControllerString(controllerString);
-        let Controller = require(controllerConfig.path);
-        let control = new Controller();
-
-        this.express.delete(path, control[controllerConfig.method]);
+        this.express.delete(path, this.getControllerMethod(controllerString));
     }
 
 
+    /**
+     * Method for grabbing the method mentioned in the
+     * controller string and caching existing controllers
+     * @param {String} controllerString
+     * @return {Function} - The method from the controller
+     */
     getControllerMethod(controllerString) {
         let controllerConfig = this.parseControllerString(controllerString);
         let path = controllerConfig.path;
+        let method = controllerConrig.method;
         let arrayIndex = this.controllerMapping[path];
 
         if(arrayIndex != undefined) {
-            return this.controllers[arrayIndex][controllerConfig.method];
+            return this.controllers[arrayIndex][method];
         }else {
             this.controllerMapping[path] = this.controllers.length;
             let Controller = require(path);
             this.controllers.push(new Controller);
-            return this.controllers[this.controllers.length-1][controllerConfig.method];
+            return this.controllers[this.controllers.length-1][method];
         }
     }
 
