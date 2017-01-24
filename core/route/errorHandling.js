e31045d628f72b8d2345b258d67d44f081364a2b
kiricon <kiricon@live.com>
2017-01-23 19:42:46 -0700

    We are now outputting broken code chunk.

--- core/route/errorHandling.js
+++ core/route/errorHandling.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 module.exports.setErrors = (app, config) => {
     app.use((req, res, next) => {
         res.status(404).render(config.Error404);
@@ -10,8 +12,21 @@ module.exports.setErrors = (app, config) => {
         let filePath = errorBreakDown[0];
         let fileLine = errorBreakDown[1];
         let fileColumn = errorBreakDown[2];
-        
+
+        let fileByLine= fs.readFileSync(filePath, 'utf8').split('\n');
+
+        let displayCode = '';
+
+        let topBuffer = fileLine > 10 ? fileLine -10 : 0;
+
+        let bottomBuffer = fileByLine.length > fileLine+10? fileLine+10 : fileByLine.length;
+
+        for(let i = topBuffer; i <= bottomBuffer; i++) {
+            displayCode += fileByLine[i]+'\n';
+        }
+
+        console.log(fileByLine);
         console.log(filePath);
-        res.status(500).render(config.Error500, {errors: errorArray});
+        res.status(500).render(config.Error500, {errors: errorArray, code: displayCode});
     });
 };
