89a3a00a762f845beeae9d1563054f9ac4fc9d47
Ivan Mattie <mattieivan@gmail.com>
2017-01-24 20:11:22 -0700

    Switched tester page to GET

--- demo/app/controllers/DatabaseController.js
+++ demo/app/controllers/DatabaseController.js
@@ -2,20 +2,15 @@ const Database = require(process.cwd() + '/../core.js').Database;
 
 class DatabaseController {
     index(req, res) {
-        res.render('db/index');
-    }
-
-    find(req, res) {
-        console.log(req.params);
-        console.log(req.body);
-        console.log(req.query);
-        console.log(req.param("id"));
-
-        res.send("What");
-
-        /*res.render('db/index', {
-            result: null
-        });*/
+        if(req.query.id) {
+            Database.table("test").find(req.query.id).then(function(data) {
+                res.render('db/index', {
+                    result: data
+                });
+            });
+        } else {
+            res.render('db/index');
+        }
     }
 }
 
--- demo/app/routes/routes.js
+++ demo/app/routes/routes.js
@@ -12,7 +12,6 @@ Route.get('/dog', 'hotdog@dog');
 
 // Database
 Route.get('/db', 'DatabaseController@index');
-Route.post('/db', 'DatabaseController@find');
 
 require('./hotdog/dog');
 
