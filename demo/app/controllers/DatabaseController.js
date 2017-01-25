a6904bad105f09c13c49fdf20a624c6286ed9584
Ivan Mattie <mattieivan@gmail.com>
2017-01-24 19:59:04 -0700

    What is going on with Express I have no idea

--- demo/app/controllers/DatabaseController.js
+++ demo/app/controllers/DatabaseController.js
@@ -4,6 +4,19 @@ class DatabaseController {
     index(req, res) {
         res.render('db/index');
     }
+
+    find(req, res) {
+        console.log(req.params);
+        console.log(req.body);
+        console.log(req.query);
+        console.log(req.param("id"));
+
+        res.send("What");
+
+        /*res.render('db/index', {
+            result: null
+        });*/
+    }
 }
 
 module.exports = DatabaseController;
