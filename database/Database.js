bdd5d3bd21138b67b4fc27d1b1a744382462d91e
Ivan Mattie <mattieivan@gmail.com>
2017-01-25 18:36:33 -0700

    Added some more documentation comments

--- database/Database.js
+++ database/Database.js
@@ -3,6 +3,14 @@ const DriverAliases = require("./drivers/DriverAliases.js");
 
 class Database {
 
+    /**
+     * Retrieves the database driver based on what the user has set in their 
+     * environment JSON file. If the driver cannot be found, will default to 
+     * the MySQL driver.
+     * 
+     * @returns {Driver} the database driver required to actually access the 
+     * database.
+     */
     static retrieveDriver() {
         let driverType = env("database.driver");
         
@@ -15,6 +23,15 @@ class Database {
         }
     }
 
+    /**
+     * Builds and returns a Table object that's used to access a specific table 
+     * or collection.
+     * 
+     * @param {string} name the name of the table to bind on
+     * 
+     * @returns {Table} the table object to use to make queries against the
+     * table
+     */
     static table(name) {
         let driver = this.retrieveDriver();
 
