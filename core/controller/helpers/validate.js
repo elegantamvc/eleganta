934b709ed100b054af3367fae33bb35edad7bc27
kiricon <kiricon@live.com>
2017-01-28 22:36:32 -0700

    Created analyze method for handling rules and values

--- core/controller/helpers/validate.js
+++ core/controller/helpers/validate.js
@@ -1,6 +1,17 @@
 module.exports = (request, rules) => {
     for(let property in rules) {
-        console.log(property+' : '+rules[property]);
+        analyze(request.body[property], rules[property]);
     }
 };
+/**
+ * Method for analzing a rule
+ * @param {Any} value - value to check rule against
+ * @param {String} rule - rule string
+ */
+function analyze(value, rule) {
+    let conditions = rule.split('|');
+    conditions.forEach( (condition) => {
+
+    });
+}
 
