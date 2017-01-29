62aec1b4e7518a18f5d83505990bd5c4b264be60
kiricon <kiricon@live.com>
2017-01-29 11:40:38 -0700

    Added required method for validation.

--- core/controller/helpers/validate.js
+++ core/controller/helpers/validate.js
@@ -1,17 +1,33 @@
 module.exports = (request, rules) => {
     for(let property in rules) {
-        analyze(request.body[property], rules[property]);
+        analyze(property, request.body[property], rules[property]);
     }
 };
 /**
  * Method for analzing a rule
+ * @param {String} name - name of value
  * @param {Any} value - value to check rule against
  * @param {String} rule - rule string
  */
-function analyze(value, rule) {
+function analyze(name, value, rule) {
+    rule = rule.toLowerCase();
     let conditions = rule.split('|');
-    conditions.forEach( (condition) => {
+    let errors = [];
 
+    conditions.forEach( (condition) => {
+        let error = '';
+        switch(condition) {
+            case 'required':
+            error = required(name, value);
+
+        }      
+        if(error != '') {
+            errors.push(error);
+        }
     });
+}
+
+function required(name, value) {
+    return value != null && value != '' ? '' : name+' cannot be null or empty.';
 }
 
